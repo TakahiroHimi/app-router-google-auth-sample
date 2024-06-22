98cc28190c28935e3e00d987865edc73c13d1faf
TakahiroHimi <takahiro.mth.1209@gmail.com>
2024-06-22 17:42:57 +0900

    sign outとユーザー情報表示を実装

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com"], // Googleのプロフィール画像を表示するために追加
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,53 @@
-import { auth, signIn } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
+import { SessionProvider } from "next-auth/react";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  if (session?.user) {
+    session.user = {
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image,
+    };
+  }
+
   return (
-    <main>
-      <form
-        action={async () => {
-          "use server";
-          await signIn("google");
-        }}
-      >
-        <button>Sign in with Google</button>
-      </form>
-    </main>
+    <SessionProvider basePath={"/auth"} session={session}>
+      <main>
+        {!session ? (
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google");
+            }}
+          >
+            <button>Sign in with Google</button>
+          </form>
+        ) : (
+          <>
+            <Image
+              alt="ユーザーアイコン"
+              src={session?.user?.image ?? ""}
+              width={24}
+              height={24}
+            />
+            <br />
+            name: {session?.user?.name}
+            <br />
+            email: {session?.user?.email}
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+              className="w-full"
+            >
+              <button>Sign Out</button>
+            </form>
+          </>
+        )}
+      </main>
+    </SessionProvider>
   );
 }
