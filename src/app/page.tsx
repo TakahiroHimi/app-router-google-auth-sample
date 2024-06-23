3ce7e4768a1a9bd8ea5cfbd2fb933a7a9543339a
TakahiroHimi <takahiro.mth.1209@gmail.com>
2024-06-23 15:10:48 +0900

    不要なSessionProviderを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { auth, signIn, signOut } from "@/auth";
-import { SessionProvider } from "next-auth/react";
 import Image from "next/image";
 
 export default async function Home() {
@@ -13,41 +12,39 @@ export default async function Home() {
   }
 
   return (
-    <SessionProvider basePath={"/auth"} session={session}>
-      <main>
-        {!session ? (
+    <main>
+      {!session ? (
+        <form
+          action={async () => {
+            "use server";
+            await signIn("google");
+          }}
+        >
+          <button>Sign in with Google</button>
+        </form>
+      ) : (
+        <>
+          <Image
+            alt="ユーザーアイコン"
+            src={session?.user?.image ?? ""}
+            width={24}
+            height={24}
+          />
+          <br />
+          name: {session?.user?.name}
+          <br />
+          email: {session?.user?.email}
           <form
             action={async () => {
               "use server";
-              await signIn("google");
+              await signOut();
             }}
+            className="w-full"
           >
-            <button>Sign in with Google</button>
+            <button>Sign Out</button>
           </form>
-        ) : (
-          <>
-            <Image
-              alt="ユーザーアイコン"
-              src={session?.user?.image ?? ""}
-              width={24}
-              height={24}
-            />
-            <br />
-            name: {session?.user?.name}
-            <br />
-            email: {session?.user?.email}
-            <form
-              action={async () => {
-                "use server";
-                await signOut();
-              }}
-              className="w-full"
-            >
-              <button>Sign Out</button>
-            </form>
-          </>
-        )}
-      </main>
-    </SessionProvider>
+        </>
+      )}
+    </main>
   );
 }
